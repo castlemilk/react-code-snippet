02dbbbfd616c79eb9c0fb6d351a624c4a65b0429
Ben Ebsworth <ben.ebsworth@gmail.com>
2019-05-26 19:08:57 +1000

    completed skeleton layout

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,7 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require("customize-cra");
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true,
-  }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: {}
-  })
+    modifyVars: {},
+  }),
 );
---
+++ src/CodeSnippet.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+interface ICodeSNippet {}
+const CodeSnippet: React.FC<ICodeSNippet> = props => {
+  return <div>hello</div>;
+};
+
+export default CodeSnippet;
